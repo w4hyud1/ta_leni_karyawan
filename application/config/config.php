2807addf3d344545be07ff33892071e39a80bdef
wahyu <wahyudi.bayunk@gmail.com>
2020-05-08 09:54:44 +0700

    edit config.php for base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/leni_karyawan/';
+$config['base_url'] = 'http://localhost/ta_leni_karyawan/';
 
 /*
 |--------------------------------------------------------------------------
